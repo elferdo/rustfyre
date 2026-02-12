31bf8c04ef7e0e13a6253dc56d309d4b8727a67c
ElFerdo <dev@fhcarron.com>
2026-02-12 20:16:48 +0100

    Working with contrast

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,11 @@ enum AppError {
     ImageSave,
 }
 
+fn contrast(u: f64) -> f64 {
+    let x = u * 2.0 - 1.0;
+    (1.0 + (x - x.powi(3) / 3.0) * 1.5) / 2.0
+}
+
 fn main() -> Result<(), Report<AppError>> {
     let init_state = DeJongState::default();
 
@@ -52,24 +57,18 @@ fn main() -> Result<(), Report<AppError>> {
         d,
     };
 
-    let mut array = Array2D::filled_with(0u32, 2160, 3840);
+    let mut array = Array2D::filled_with(1u32, 2160, 3840);
 
-    let mut coords = Vec::with_capacity(1000000000);
+    let xmin = -2.0;
+    let xmax = 2.0;
+    let ymin = -2.0;
+    let ymax = 2.0;
 
-    for _ in 1..1000000000 {
+    for _ in 1..10000000000u64 {
         let state = dj.next().unwrap();
 
-        coords.push(state);
-    }
-
-    let xmin = coords.iter().fold(0.0f64, |b, s| b.min(s.x));
-    let xmax = coords.iter().fold(0.0f64, |b, s| b.max(s.x));
-    let ymin = coords.iter().fold(0.0f64, |b, s| b.min(s.y));
-    let ymax = coords.iter().fold(0.0f64, |b, s| b.max(s.y));
-
-    for c in coords {
-        let x4k = ((c.x - xmin) * 3839.0 / (xmax - xmin)) as usize;
-        let y4k = ((c.y - ymin) * 2159.0 / (ymax - ymin)) as usize;
+        let x4k = ((state.x - xmin) * 3839.0 / (xmax - xmin)) as usize;
+        let y4k = ((state.y - ymin) * 2159.0 / (ymax - ymin)) as usize;
 
         array[(y4k, x4k)] = array[(y4k, x4k)] + 1;
     }
@@ -77,12 +76,17 @@ fn main() -> Result<(), Report<AppError>> {
     let v: Vec<_> = array
         .as_row_major()
         .into_iter()
-        .map(|v| (v as f64).log10())
+        .map(|v| (v as f64).log2())
         .collect();
 
     let max_v = v.iter().fold(0.0f64, |b, x| b.max(*x));
 
-    let v2: Vec<_> = v.into_iter().map(|v| (v * 255.0 / max_v) as u8).collect();
+    let v2: Vec<_> = v
+        .into_iter()
+        .map(|v| (255.0 - contrast(v / max_v) * 255.0) as u8)
+        .collect();
+
+    let color = OpaqueColor::<OkLab>::new();
 
     let buffer = GrayImage::from_vec(3840, 2160, v2).unwrap();
 
