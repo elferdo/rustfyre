7706a36e37ee243ffbc57fa71c62c8620db2dc3d
ElFerdo <dev@fhcarron.com>
2026-02-12 10:49:32 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use array2d::Array2D;
+use error_stack::{Report, ResultExt};
+use image::GrayImage;
+use thiserror::Error;
+
+#[derive(Copy, Clone, Debug, Default)]
+struct DeJongState {
+    x: f64,
+    y: f64,
+}
+
+struct DeJong {
+    state: DeJongState,
+    a: f64,
+    b: f64,
+    c: f64,
+    d: f64,
+}
+
+impl Iterator for DeJong {
+    type Item = DeJongState;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let new_x = (self.a * self.state.y).sin() - (self.b * self.state.x).cos();
+        let new_y = (self.c * self.state.x).sin() - (self.d * self.state.y).cos();
+
+        self.state = DeJongState { x: new_x, y: new_y };
+
+        Some(self.state)
+    }
+}
+
+#[derive(Debug, Error)]
+enum AppError {
+    #[error("saving image")]
+    ImageSave,
+}
+
+fn main() -> Result<(), Report<AppError>> {
+    let init_state = DeJongState::default();
+
+    let a = -2.7;
+    let b = -0.9;
+    let c = -0.86;
+    let d = -2.2;
+
+    let mut dj = DeJong {
+        state: init_state,
+        a,
+        b,
+        c,
+        d,
+    };
+
+    let mut array = Array2D::filled_with(0u32, 2160, 3840);
+
+    let mut coords = Vec::with_capacity(1000000000);
+
+    for _ in 1..1000000000 {
+        let state = dj.next().unwrap();
+
+        coords.push(state);
+    }
+
+    let xmin = coords.iter().fold(0.0f64, |b, s| b.min(s.x));
+    let xmax = coords.iter().fold(0.0f64, |b, s| b.max(s.x));
+    let ymin = coords.iter().fold(0.0f64, |b, s| b.min(s.y));
+    let ymax = coords.iter().fold(0.0f64, |b, s| b.max(s.y));
+
+    for c in coords {
+        let x4k = ((c.x - xmin) * 3839.0 / (xmax - xmin)) as usize;
+        let y4k = ((c.y - ymin) * 2159.0 / (ymax - ymin)) as usize;
+
+        array[(y4k, x4k)] = array[(y4k, x4k)] + 1;
+    }
+
+    let v: Vec<_> = array
+        .as_row_major()
+        .into_iter()
+        .map(|v| (v as f64).log10())
+        .collect();
+
+    let max_v = v.iter().fold(0.0f64, |b, x| b.max(*x));
+
+    let v2: Vec<_> = v.into_iter().map(|v| (v * 255.0 / max_v) as u8).collect();
+
+    let buffer = GrayImage::from_vec(3840, 2160, v2).unwrap();
+
+    buffer
+        .save("hola.png")
+        .change_context(AppError::ImageSave)?;
+
+    Ok(())
+}
